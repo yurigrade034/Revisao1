52d9378530e7635d4ad44018fc7be2fcb294540c
yurigrade034 <gradeyuri1@gmail.com>
2026-08-19 22:22:49 -0300

    atualizar

--- src/repositorio/JPA.kt
+++ src/repositorio/JPA.kt
@@ -36,13 +36,28 @@ class JPA(
         println("Salvando...")
         try {
             conectar()//abre a conexao com o banco
-            conexao!!.createStatement().executeQuery(
-                        "INSERT INTO caixa_da_agua " +
+            val sql = "INSERT INTO caixa_da_agua " +
                         "(marca, modelo, dimensao, cor, material, formato, preco) " +
-                        "VALUES (${a.marca}, ${a.modelo}, ${a.dimensao}, ${a.cor}, ${a.material}, ${a.formato}, ${a.preco}) "
-            )
-            conexao!!.close()//encerra a conexao com o banco
+                        "VALUES (?, ?, ?, ?, ?, ?, ?) "
 
+            val stmt = conexao!!.prepareStatement(sql)
+
+            //preparar lista para double precision
+            val doublePrecision = conexao!!.createArrayOf("float8", a.dimensao.toTypedArray())
+            //o typedArray() converte o array para um tipo de dado legivel para o POSTGRESQL
+
+            //Preparar as variaveis para o banco
+            stmt.setString(1,a.marca)
+            stmt.setString(2,a.modelo)
+            stmt.setArray(3,doublePrecision)
+            stmt.setString(4,a.cor.name)
+            stmt.setString(5,a.material.name)
+            stmt.setString(6,a.formato)
+            stmt.setString(7,a.preco.toString())
+            stmt.executeUpdate()
+
+            stmt.close()//encerra o placeholder
+            conexao!!.close()//encerra a conexao com o banco
         } catch (e: SQLException) {
             println("F total parceiro: ${e.printStackTrace()}")
         }
